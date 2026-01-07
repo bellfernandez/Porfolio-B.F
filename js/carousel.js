ccfdc180ad16ac37d939552375f37e00ae4bd210
belen <bel.f_91@hotmail.com>
2026-01-07 19:38:15 -0300

    portfolio final

--- js/carousel.js
+++ js/carousel.js
@@ -4,17 +4,25 @@ const slides = document.querySelectorAll(".project");
 const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 
+const visibleCards = 3;
+
 function updateCarousel() {
-  const width = slides[0].offsetWidth + 40;
-  track.style.transform = `translateX(-${index * width}px)`;
+  const cardWidth = slides[0].offsetWidth + 30;
+  track.style.transform = `translateX(-${index * cardWidth}px)`;
 }
 
 nextBtn.addEventListener("click", () => {
-  index = (index + 1) % slides.length;
-  updateCarousel();
+  if (index < slides.length - visibleCards) {
+    index++;
+    updateCarousel();
+  }
 });
 
 prevBtn.addEventListener("click", () => {
-  index = (index - 1 + slides.length) % slides.length;
-  updateCarousel();
+  if (index > 0) {
+    index--;
+    updateCarousel();
+  }
 });
+
+window.addEventListener("resize", updateCarousel);
